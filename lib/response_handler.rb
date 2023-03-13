d9fbc9d522a3f260093e9fc3c5fac18cf5cd3741
Mike Kokotov <ruuke@mail.ru>
2023-03-13 15:18:10 +0600

    fix double case

--- lib/response_handler.rb
+++ lib/response_handler.rb
@@ -13,15 +13,12 @@ module ResponseHandler
       response
     when 300..399
       raise RedirectionError, "#{class_name}::Error Unexpected redirection from api response."
-    when 400..499
-      case response.status
-      when 400
-        raise ClientError, "#{class_name}::Error Bad request: #{response.body}"
-      when 401
-        raise ClientError, "#{class_name}::Error Unauthorized request: #{response.headers['www-authenticate']}"
-      else
-        raise ClientError, "#{class_name}::Error Bad request: #{response.body}"
-      end
+    when 400
+      raise ClientError, "#{class_name}::Error Bad request: #{response.body}"
+    when 401
+      raise ClientError, "#{class_name}::Error Unauthorized request: #{response.headers['www-authenticate']}"
+    when 402..499
+      raise ClientError, "#{class_name}::Error Bad request: #{response.body}"
     when 500..599
       raise ServerError, "#{class_name}::Error #{class_name} server error: #{response.body}"
     end
